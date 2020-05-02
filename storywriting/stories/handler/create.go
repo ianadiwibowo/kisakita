19276fe5ccc217427b62ca3616e6437fc20289c0
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-02 17:00:22 +0700

    Add request payload validation in stories create handler

--- storywriting/stories/handler/create.go
+++ storywriting/stories/handler/create.go
@@ -1,14 +1,26 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // Create handles POST /stories
 func (h *StoriesHandler) Create(w http.ResponseWriter, r *http.Request) {
 	// Validate request payload
+	var p createParams
+	getParams(r, &p)
+	errs := validateParams(p)
+	if errs != nil {
+		respondBadRequest(w, errs, nil)
+		return
+	}
 
-	// Process to usecase
+	// Process according to business rules
+	fmt.Println("Process according to business rules")
+}
 
-	// Respond
+type createParams struct {
+	Title    string `validate:"required"`
+	Synopsis string `validate:"required"`
 }
