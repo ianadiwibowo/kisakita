79ac17f76ee8eaa0f8c25f66108f7979515167d3
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-05 00:15:24 +0700

    Put repository in usecase

--- storywriting/stories/usecase/base.go
+++ storywriting/stories/usecase/base.go
@@ -1,8 +1,17 @@
 package usecase
 
-type StoryUsecase struct{}
+import (
+	"gitlab.com/ianadiwibowo/kisakita/storywriting"
+	"gitlab.com/ianadiwibowo/kisakita/storywriting/stories/repository"
+)
+
+type StoryUsecase struct {
+	StoryRepository storywriting.StoryRepository
+}
 
 // NewStoryUsecase initializes a new StoryUsecase instance
 func NewStoryUsecase() *StoryUsecase {
-	return &StoryUsecase{}
+	return &StoryUsecase{
+		StoryRepository: repository.NewStoryRepository(),
+	}
 }
