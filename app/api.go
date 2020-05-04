01dcee13768cae653aca32f6976e6b46ab02cfde
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-04 23:38:57 +0700

    DRY-ing main partition

--- app/api.go
+++ app/api.go
@@ -16,6 +16,43 @@ type App struct {
 	Router *mux.Router
 }
 
+// New initializes the app
+func NewApp() *App {
+	return &App{
+		Router: mux.NewRouter().StrictSlash(true),
+	}
+}
+
+// SetupRoutes initializes the API endpoint routes
+func (app *App) SetupRoutes() {
+	app.useBasicAuth()
+
+	h := handler.NewStoriesHandler()
+	app.route("GET", "/stories/{id}", h.GetByID)
+	app.route("POST", "/stories", h.Create)
+	app.route("PATCH", "/stories/{id}", h.Update)
+	app.route("DELETE", "/stories/{id}", h.Delete)
+	app.route("GET", "/stories/by-authors/{id}", h.GetByAuthorID)
+	app.route("GET", "/stories/ten-latests", h.Get10LatestStories)
+}
+
+// useBasicAuth add API endpoint authentication using basic auth
+func (app *App) useBasicAuth() {
+	app.Router.Use(httpauth.SimpleBasicAuth(
+		os.Getenv("BASIC_USERNAME"),
+		os.Getenv("BASIC_PASSWORD"),
+	))
+}
+
+// route registers an API endpoint to a handler
+func (app *App) route(
+	method string,
+	path string,
+	handler func(http.ResponseWriter, *http.Request),
+) {
+	app.Router.Methods(method).Path(path).HandlerFunc(handler)
+}
+
 func main() {
 	loadEnvironmentVariables()
 
@@ -33,41 +70,4 @@ func loadEnvironmentVariables() {
 			panic(err)
 		}
 	}
-}
-
-// New initializes the app
-func NewApp() *App {
-	return &App{
-		Router: mux.NewRouter().StrictSlash(true),
-	}
-}
-
-// SetupRoutes initializes the API endpoint routes
-func (app *App) SetupRoutes() {
-	// Authenticate using basic auth, for now
-	app.Router.Use(httpauth.SimpleBasicAuth(
-		os.Getenv("BASIC_USERNAME"),
-		os.Getenv("BASIC_PASSWORD"),
-	))
-
-	storywritingHandler := handler.NewStoriesHandler()
-
-	app.Router.Methods("GET").Path("/stories/{id}").HandlerFunc(
-		storywritingHandler.GetByID,
-	)
-	app.Router.Methods("POST").Path("/stories").HandlerFunc(
-		storywritingHandler.Create,
-	)
-	app.Router.Methods("PATCH").Path("/stories/{id}").HandlerFunc(
-		storywritingHandler.Update,
-	)
-	app.Router.Methods("DELETE").Path("/stories/{id}").HandlerFunc(
-		storywritingHandler.Delete,
-	)
-	app.Router.Methods("GET").Path("/stories/by-authors/{id}").HandlerFunc(
-		storywritingHandler.GetByAuthorID,
-	)
-	app.Router.Methods("GET").Path("/stories/ten-latests").HandlerFunc(
-		storywritingHandler.Get10LatestStories,
-	)
 }
