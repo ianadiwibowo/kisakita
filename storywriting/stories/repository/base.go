d54cbc84113770fd00c43f64cd812c7f5d9852d1
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-05 01:41:55 +0700

    Add DB initialization in base repository

--- storywriting/stories/repository/base.go
+++ storywriting/stories/repository/base.go
@@ -1,8 +1,35 @@
 package repository
 
-type StoryRepository struct{}
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+type StoryRepository struct {
+	DB *gorm.DB
+}
 
 // NewStoryRepository initializes a new StoryRepository instance
 func NewStoryRepository() *StoryRepository {
-	return &StoryRepository{}
+	connectionString := fmt.Sprintf(
+		"%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		os.Getenv("DATABASE_USERNAME"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"),
+	)
+
+	db, err := gorm.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatalf("Database connection failed: %s", err.Error())
+	}
+
+	return &StoryRepository{
+		DB: db,
+	}
 }
