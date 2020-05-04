a98c29597ae53f80f9caceab77119b99b65f0f8a
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-05 00:34:38 +0700

    Complete create story handler

--- storywriting/stories/handler/create.go
+++ storywriting/stories/handler/create.go
@@ -1,23 +1,36 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
+
+	"gitlab.com/ianadiwibowo/kisakita/entity"
 )
 
-// Create handles POST /stories
+// Create handles `POST /stories`
 func (h *StoriesHandler) Create(w http.ResponseWriter, r *http.Request) {
 	// Validate request payload
-	var p createParams
-	getParams(r, &p)
-	errs := validateParams(p)
+	var params createParams
+	getParams(r, &params)
+	errs := validateParams(params)
 	if errs != nil {
 		respondBadRequest(w, errs, nil)
 		return
 	}
 
 	// Process according to business rules
-	fmt.Println("Process according to business rules")
+	err := h.StoryUsecase.Create(
+		&entity.Story{
+			Title:    params.Title,
+			Synopsis: params.Synopsis,
+		},
+	)
+	if err != nil {
+		respondUnprocessableEntity(w, []error{err}, nil)
+		return
+	}
+
+	// Send success response
+	respondOK(w, nil, nil)
 }
 
 type createParams struct {
