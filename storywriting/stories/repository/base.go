c9b84c93cf93661b38c76fe6d2026d3193f4a38c
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-04 23:46:14 +0700

    Add base story repository

---
+++ storywriting/stories/repository/base.go
@@ -0,0 +1,8 @@
+package repository
+
+type StoryRepository struct{}
+
+// NewStoryRepository initializes a new StoryRepository instance
+func NewStoryRepository() *StoryRepository {
+	return &StoryRepository{}
+}
