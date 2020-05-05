5cf4014a1e99abb143a0231180ffa24ccfdfcb1b
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-06 01:46:28 +0700

    Implement story repository create function

--- storywriting/stories/repository/create.go
+++ storywriting/stories/repository/create.go
@@ -1,10 +1,23 @@
 package repository
 
 import (
+	"time"
+
 	"gitlab.com/ianadiwibowo/kisakita/entity"
 )
 
 // Create saves the newStory
 func (r *StoryRepository) Create(newStory *entity.Story) error {
+	createTimestamp(newStory)
+
+	err := r.db.Create(newStory).Error
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func createTimestamp(story *entity.Story) {
+	story.CreatedAt = time.Now()
+	story.UpdatedAt = story.CreatedAt
 }
