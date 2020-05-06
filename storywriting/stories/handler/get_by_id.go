adf015bd0889c64791ff5666926cc83c2185bedb
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-07 02:07:39 +0700

    Implement GetByID handler

--- storywriting/stories/handler/get_by_id.go
+++ storywriting/stories/handler/get_by_id.go
@@ -2,7 +2,28 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // Show handles `GET /stories/{id}`
-func (h *StoriesHandler) GetByID(w http.ResponseWriter, r *http.Request) {}
+func (h *StoriesHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	// Validate request payload
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+	if id == 0 || err != nil {
+		respondUnprocessableEntity(w, []error{err}, nil)
+		return
+	}
+
+	// Process according to business rules
+	story, err := h.StoryUsecase.GetByID(id)
+	if err != nil {
+		respondUnprocessableEntity(w, []error{err}, nil)
+		return
+	}
+
+	respondOK(w, story, nil)
+}
