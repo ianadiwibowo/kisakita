c96b7dfac1f803204d9f643c13c143708507858f
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-07 02:07:23 +0700

    Add GetByID usecase

--- storywriting/stories/usecase/get_by_id.go
+++ storywriting/stories/usecase/get_by_id.go
@@ -4,5 +4,9 @@ import "gitlab.com/ianadiwibowo/kisakita/entity"
 
 // GetByID retrieves a single story by the storyID
 func (u *StoryUsecase) GetByID(storyID int) (story *entity.Story, err error) {
-	return nil, nil
+	story, err = u.StoryRepository.GetByID(storyID)
+	if err != nil {
+		return nil, err
+	}
+	return story, nil
 }
