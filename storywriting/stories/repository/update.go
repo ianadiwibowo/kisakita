8f9dd689b3ea507273552fbac5b0c0d9e704c98d
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-07 02:15:26 +0700

    Create draft update repository

--- storywriting/stories/repository/update.go
+++ storywriting/stories/repository/update.go
@@ -4,7 +4,23 @@ import (
 	"gitlab.com/ianadiwibowo/kisakita/entity"
 )
 
-// Update edits the existingStory
-func (r *StoryRepository) Update(existingStory *entity.Story) error {
+// Update edits the updatedStory
+func (r *StoryRepository) Update(
+	updatedStory *entity.Story,
+) (err error) {
+	// Retrieve first
+	var story entity.Story
+	err = r.db.First(&story, updatedStory.ID).Error
+	if err != nil {
+		return err
+	}
+
+	// Update
+	story = *updatedStory
+	err = r.db.Save(&story).Error
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
