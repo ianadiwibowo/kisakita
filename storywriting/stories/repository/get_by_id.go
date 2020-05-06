02f5c63c992d2cc9a085ec478c89872b42c0e745
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-07 02:07:08 +0700

    Add GetByID repository

--- storywriting/stories/repository/get_by_id.go
+++ storywriting/stories/repository/get_by_id.go
@@ -1,8 +1,18 @@
 package repository
 
-import "gitlab.com/ianadiwibowo/kisakita/entity"
+import (
+	"gitlab.com/ianadiwibowo/kisakita/entity"
+)
 
 // GetByID retrieves a single story by the storyID
-func (r *StoryRepository) GetByID(storyID int) (story *entity.Story, err error) {
-	return nil, nil
+func (r *StoryRepository) GetByID(storyID int) (
+	*entity.Story,
+	error,
+) {
+	story := entity.Story{}
+	err := r.db.First(&story, storyID).Error
+	if err != nil {
+		return nil, err
+	}
+	return &story, nil
 }
