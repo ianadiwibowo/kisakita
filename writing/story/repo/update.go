1dc6acea114c35ebfb8926de2260f820695aaa01
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-05-07 16:45:43 +0700

    Fix undeclared variable typo

--- writing/story/repo/update.go
+++ writing/story/repo/update.go
@@ -8,7 +8,7 @@ import (
 func (r *StoryRepo) Update(updatedStory *entity.Story) error {
 	// Retrieve first
 	var story entity.Story
-	err = r.db.First(&story, updatedStory.ID).Error
+	err := r.db.First(&story, updatedStory.ID).Error
 	if err != nil {
 		return err
 	}
